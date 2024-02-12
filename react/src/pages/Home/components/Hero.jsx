ba4280a0a83f262df449bb44955d71e834b26d45
HishamLadha <hishamladha@gmail.com>
2024-02-11 20:22:45 -0500

    Working on hero section

--- react/src/pages/Home/components/Hero.jsx
+++ react/src/pages/Home/components/Hero.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div className='flex m-8'>
+      
+      <div>
+      <img src="./assets/graphics/herotext.png" className="" alt="Concordia Building" />
+      </div>
+
+      <div className='ml-auto'>
+        <img src="./assets/graphics/concbuilding.png" className="" alt="Concordia Building" />
+        <span className=''>Sign up to newsletter</span>
+      </div>
+    
+    </div>
   )
 }
 
