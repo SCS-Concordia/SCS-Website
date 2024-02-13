a57253db6971f1487b4dfd79ff912bb89a1d30e4
HishamLadha <hishamladha@gmail.com>
2024-02-12 21:39:06 -0500

    Worked on Hero section, hero text, added animations. Responsiveness to be worked on.

--- react/src/pages/Home/components/Hero.jsx
+++ react/src/pages/Home/components/Hero.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
+import '../../../index.css';
+import { motion } from 'framer-motion';
 
 function Hero() {
   return (
-    <div className='flex m-8'>
+    <div className='flex m-8 items-center justify-center gap-x-12'>
       
-      <div className="">
-      <img src="./assets/graphics/herotext3.png" className="h-96" alt="Concordia Building" />
+      {/* Hero text */}
+      
+      <div className="relative w-1/2">
+        {/* Black text (shadow/border effect) */}
+        <h1 className='font-ibmPlex text-[4.51em] drop-shadow-lg text-black absolute' style={{ top: '-2px', left: '-5px' }}>
+          Software Engineering and Computer Science
+        </h1>
+        {/* White text */}
+        <h1 className='font-ibmPlex text-[4.5em] drop-shadow-lg text-white relative'>
+          Software Engineering and Computer Science<span className='blinker text-gray-200 drop-shadow-lg font-ibmPlex'>|</span>
+        </h1>
+        
       </div>
-
-      <div className='ml-auto'>
+      
+      
+      <motion.div initial={{x: "100%" }} animate={{ x: "-5%" }} transition={{duration: 0.7}}>
+      <div className=''>
         <img src="./assets/graphics/concbuilding.png" className="" alt="Concordia Building" />
         <span className=''>Sign up to newsletter</span>
       </div>
+      </motion.div>
     
     </div>
   )
