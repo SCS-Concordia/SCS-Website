64d57808a3032da039fe6b17d980068ec7a1576e
DobDub <hassan.moharram@hotmail.com>
2024-03-02 13:43:20 -0500

    Added router and skeleton of events

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home.jsx';
+import Events from './pages/Events/Events.jsx';
 import Nav from './Nav.jsx';
 
 function App() {
   return (
-    <div className='bg-gradient-to-b from-light-purple to-dark-purple min-h-screen'>
+    <Router>
+      <div className='bg-gradient-to-b from-light-purple to-dark-purple min-h-screen'>
         <Nav />
-        <Home />
-    </div>
-  )
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/events' element={<Events />} />
+          {/* Add other routes if needed */}
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ react/src/pages/Events/components/Accordion.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+const Accordion = () => {
+    <>test</>
+};
+
+export default Accordion;
